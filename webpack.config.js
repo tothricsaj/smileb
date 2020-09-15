aad486e6c2b9d38081772105d90096db603a1e7d
tothricsaj <tothricsaj@gmail.com>
2020-09-15 11:50:06 +0200

    some dev staff

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
 
 module.exports = {
     entry: {
         index: './src/index.js',
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html'
+        })
+    ],
     output: {
         path: path.resolve(__dirname, 'dist/'),
         filename: '[name].js'
+    },
+    devServer: {
+        contentBase: './dist',
     },
 }
