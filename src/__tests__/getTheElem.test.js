a9130c061c07581e7116aa540aec0b03ff034693
tothricsaj <tothricsaj@gmail.com>
2020-09-15 13:45:27 +0200

    getTheElement test and implementation

--- src/__tests__/getTheElem.test.js
+++ src/__tests__/getTheElem.test.js
@@ -1,5 +1,17 @@
+document.body.innerHTML = `
+    <div>
+        <p class="smileBInput">foo bar baz</p>
+    </div>
+`
+
+// <input class="smileBInput" type="text" placeholder="Type your text..." />
+
 const gte = require('../view/getTheElem')
+const $ = require('jquery')
 
 describe('Testing getTheElement', () => {
-    
+    it('Get element lists', () => {
+        expect(NodeList.prototype.isPrototypeOf(gte.getTheElem())).toEqual(true)
+        expect(gte.getTheElem()).toBeTruthy()
+    })
 });
--- src/view/getTheElem.js
+++ src/view/getTheElem.js
@@ -1,7 +1,18 @@
 /**
- * This is the fires jsDoc comment by me.
- * Let's see what is the miracle.
+ * This function get the all input element which are taken the class
+ * the class is smileBInput
+ * @returns {Array}
+ * 
  */
 export function getTheElem() {
-
+    return document.querySelectorAll('.smileBInput')
 }
+
+/**
+ * Checking data-smile-flag = true
+ * @param {Object[]} elemList - The list of elements with smileBInput class
+ */
+export function checkSmileFlag(elemList) {
+    return null
+}
+
