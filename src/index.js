e02878ea1f7e7911fc4b349a93e0d58e8f90b618
tothricsaj <tothricsaj@gmail.com>
2020-09-16 13:45:27 +0200

    correct image loading wiht webpack require.context

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import {testFunc} from './view/view.js'
 import Pic from './assets/emojis/00A9.png'
 
 testFunc()
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {
+      images[item.replace('./', '')] = r(item)
+    })
+  return images
+}
+
+// require.context -> https://webpack.js.org/guides/dependency-management/
+const images = importAll(require.context('./assets/emojis', false, /\.(png|jpe?g|svg)$/))
+
 console.log(`Pic -> ${Pic}`)
-document.querySelector('#myImg').src = Pic
+document.querySelector('#myImg1').src = Pic
+document.querySelector('#myImg2').src = images['1F3A7.png'].default
+
 console.log('hello from index.js')
